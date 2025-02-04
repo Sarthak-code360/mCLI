dc99cd112f1320257738e41a0ffa87611c60e477
Sarthak-code360 <sarthakmishra3032002@gmail.com>
2025-02-04 18:33:49 +0530

    Connect server command

--- cli.py
+++ cli.py
@@ -25,3 +25,38 @@ RECEIVE_TYPES = {
     1: "immobilize",
     2: "rpmPreset",
 }
+
+# Connect to server
+def connect_to_server():
+    """Connect to the server"""
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((SERVER_IP, SERVER_PORT))
+        print(f"✅Connected to server {SERVER_IP}:{SERVER_PORT}")
+        return client
+    except Exception as e:
+        print(f"❌Failed to connect to server: {e}")
+        sys.exit(1)
+
+def main():
+    """CLI Menu to intact with server"""
+
+    while True:
+        print("\n Welcome to Mazout CLI Tool")
+        print("1. Connect")
+        print("2. Quit")
+
+        choice = input("\nEnter option: ")
+
+        if choice == "1":
+            client = connect_to_server()
+            
+        elif choice == "2":
+            print("\n Goodbye!! 👋 ")
+            sys.exit(0)
+        else:
+            print("\n ❌ Invalid option")
+
+
+if __name__ == "__main__":
+    main()
